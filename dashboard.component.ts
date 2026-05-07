e74df12f8aab8776705e6e61a63da9cea1a43498
varalysit-alt <varalysit@gmail.com>
2026-05-07 08:35:02 +0300

    Delete dashboard.component.ts

--- dashboard.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard',
-  template: '<h2>Добро пожаловать в админ-панель</h2>'
-})
-export class DashboardComponent { }
